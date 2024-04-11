5b12fd0ab5a93655a2f680a33574d3e14e923c93
rushi-git5204 <112230364+rushi-git5204@users.noreply.github.com>
2024-04-11 09:55:01 +0000

    veriables and datatypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,11 @@
+const accountId = 10
+let accountEmail = "rushikesh@gmail.com"
+var accountPassword = "123456"
+accountCity ="Sangli"
+
+//accountId = 15 // not allowed
+
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+// number => 2 to 53
+// bigint
+// string
+//boolean => true/false
+// null => standalon value
+// undefined
+// symbol => unique
+// object 
+
+
+console.log(typeof undefined) // output = undefined
+console.log(typeof null) // output = object
